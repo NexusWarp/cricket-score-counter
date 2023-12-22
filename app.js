b570fb10235e8d69bcf4056fc19b9885677715d3
NexusWarp <ik571572@gmail.com>
2023-12-22 23:29:21 +0500

    btn updates

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 let score = 0;
+document.getElementById("score").innerHTML=score;
 document.getElementById("six").addEventListener("click", ()=>{
-    
-    score += 6;
-    document.getElementById("score").innerHTML=score;
+   updateScore(6);
 })
+document.getElementById("four").addEventListener("click", ()=>{
+    updateScore(4);
+ })
+ document.getElementById("three").addEventListener("click", ()=>{
+    updateScore(3);
+ })
+ document.getElementById("two").addEventListener("click", ()=>{
+    updateScore(2);
+ })
+ document.getElementById("one").addEventListener("click", ()=>{
+    updateScore(1);
+ })
+
+function updateScore(runs){
+    score += runs;
+    document.getElementById("score").innerHTML=score;
+    console.log(score);
+}
